4238c7f2bbddabf9f48a7e820c3123c968c324de
Philip Oluwasanmi <oluwasanmiphilip26@gmail.com>
2026-04-18 03:02:59 +0100

    ValidatedStrienght

---
+++ TalentFlow.Persistence/Migrations/20260418020108_ValidationStrienghted.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TalentFlow.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ValidationStrienghted : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LastLoginToken",
+                table: "user",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastLoginToken",
+                table: "user");
+        }
+    }
+}
