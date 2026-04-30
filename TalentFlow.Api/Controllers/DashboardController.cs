320657795e1269bc697b5f56c4821cc5f06898e3
Philip Oluwasanmi <oluwasanmiphilip26@gmail.com>
2026-04-30 18:13:01 +0100

    Corrected DashBoard Error

--- TalentFlow.Api/Controllers/DashboardController.cs
+++ TalentFlow.Api/Controllers/DashboardController.cs
@@ -3,6 +3,8 @@
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
 using TalentFlow.Application.Common.Models;
 using TalentFlow.Application.Dashboard.Queries;
 
